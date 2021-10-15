ce6f910f18b0fdba3f37da0bd361ded8ea83f13a
Diogo L. Menezes <dmenezes@itelinfo.com>
2021-10-15 08:43:38 -0400

    Melhorando filtros utilizados

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const oficialFoods = []
 const rawdata = fs.readFileSync('foodList.json');
 const food = JSON.parse(rawdata);
 food.forEach(element => {
-    const newFood ={
-        nome: element.description,
-        carboidrado: element.attributes.carbohydrate && !isNaN(element.attributes.carbohydrate.qty) ? element.attributes.carbohydrate.qty : 0,
-        proteina: element.attributes.protein && !isNaN(element.attributes.protein.qty) ? element.attributes.protein.qty : 0,
-        lipid: element.attributes.lipid && !isNaN(element.attributes.lipid.qty) ? element.attributes.lipid.qty : 0,
-        kcal: element.attributes.energy && !isNaN(element.attributes.energy.kcal) ? element.attributes.energy.kcal : 0
+    if(element.attributes.lipid)
+    if(Number(element.attributes.lipid.qty)>1 && !element.description.includes('cru')){ 
+        const newFood ={
+            nome: element.description,
+            carboidrato: element.attributes.carbohydrate && !isNaN(element.attributes.carbohydrate.qty) ? element.attributes.carbohydrate.qty : 0,
+            proteina: element.attributes.protein && !isNaN(element.attributes.protein.qty) ? element.attributes.protein.qty : 0,
+            lipid: element.attributes.lipid && !isNaN(element.attributes.lipid.qty) ? element.attributes.lipid.qty : 0,
+            kcal: element.attributes.energy && !isNaN(element.attributes.energy.kcal) ? element.attributes.energy.kcal : 0
+        }
+        console.log(newFood)
+        oficialFoods.push(newFood)
     }
-    console.log(newFood)
-    oficialFoods.push(newFood)
 });
 
 const jsonOficialFood = JSON.stringify(oficialFoods)
-fs.writeFileSync('FinalFoodList.json', jsonOficialFood)
+fs.writeFileSync('foods.json', jsonOficialFood)
